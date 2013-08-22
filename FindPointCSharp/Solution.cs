a291c13801301e73119fab4041ac4fe98e318568
Erik S. Carlsten <ecarlste.register@gmail.com>
2013-08-21 18:03:35 -0600

    Added method to read number of points
    - method reads in number of points from STDIN
    - method catches FormatException if not convertable to int32
    - method returns -1 if no valid input recieved
    - Main returns if it gets -1 from method when called

---
+++ FindPointCSharp/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int numberOfPoints = ReadNumberOfPoints();
+
+        // if we recieve a -1 for the number of points, then we know an error
+        // occurred
+        if (numberOfPoints == -1)
+        {
+            return;
+        }
+    }
+
+    static int ReadNumberOfPoints()
+    {
+        int numberOfPoints = -1;
+        String line = Console.ReadLine();
+
+        // ensure that the value read in can be converted to an int32, and if
+        // it can't, then catch the FormatException thrown and let the user
+        // know what incorrect value was typed.
+        try
+        {
+            numberOfPoints = Convert.ToInt32(line);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid Input: '" + line + "' not an integer");
+        }
+
+        return numberOfPoints;
+    }
+}
